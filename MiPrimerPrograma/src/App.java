755697b0bc12c0719d33f21a2ec832c1fe1b952e
Alejandro-Alvare <ninoalvar.esp@gmail.com>
2023-11-16 16:09:51 -0600

    Se creo el proyecto

---
+++ MiPrimerPrograma/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+
+    public static void main(String[] args) {
+        String MiNombre = "Alejandro Alvarez";
+        int edad = 17;
+        float salarioBruto;
+        System.out.println(MiNombre);
+        System.out.println(edad);        
+    }
+}
